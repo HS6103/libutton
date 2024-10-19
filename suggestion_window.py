39bcc6f97b8a5a9bdcb97906d2fe75de059e1921
Robert <robert20040811@gmail.com>
2024-10-19 16:26:50 +0800

    adjust window size

--- suggestion_window.py
+++ suggestion_window.py
@@ -5,12 +5,13 @@ class SuggestionsWindow:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Suggested Action")
-        self.root.geometry("300x100")
+        self.root.geometry("150x100")
         self.root.overrideredirect(True)  # 去掉邊框
         self.root.attributes('-topmost', True)  # 視窗永遠保持在最上層
-        self.root.withdraw() # 先隱藏
+        self.root.config(bg='black')  # 設置背景顏色，方便看到透明效果
+        self.root.attributes('-transparentcolor', 'black')  # 設置黑色為透明背景
         # 創建一個標籤來顯示Suggested Action
-        self.label = tk.Label(self.root, text="Waiting for action...", font=("Arial", 12))
+        self.label = tk.Label(self.root, text="Waiting for action...", font=("Arial", 8))
         self.label.pack(pady=20)
 
     def update_label(self, input_text):
@@ -18,8 +19,8 @@ class SuggestionsWindow:
         self.root.update()
 
     def update_window_position(self, x, y):
-        offset_x = 20
-        offset_y = 20
+        offset_x = 8
+        offset_y = 2
         self.root.geometry(f"+{x + offset_x}+{y + offset_y}")
         self.root.update()
     
